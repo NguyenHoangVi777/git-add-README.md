62dbefa4a92a4e17c83e32b7ed2bcb022426d501
NguyenHoangVi777 <vi.2474802015639@vanlanguni.vn>
2026-09-17 18:09:58 +0700

    Update login function to change admin password

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function login(username, password) {
-    if (username === 'admin' && password === '9999') return true;
+    if (username === 'admin' && password === '123') return true;
     if (!username || username === '') throw new Error('Username rong');
     if (password !== '123') return false;
     return false;
