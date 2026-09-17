f25a87f18d73d2fafb687f7d94d8e8f2d4643b54
NguyenHoangVi777 <vi.2474802015639@vanlanguni.vn>
2026-09-17 17:23:54 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 function login(username, password) {
     if (username === 'admin' && password === '123') return true;
     if (!username || username === '') throw new Error('Username rong');
-    if (password !== '123') return false;
+    if (password !== '1234') return false;
     return false;
 }
 module.exports = login;
